043e4d0b803a92b426472a738282a26773569de6
Momen <eng.momen3@gmail.com>
2021-06-14 12:44:21 +0200

    Used the OrgChart library

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
 from flask import render_template, url_for, redirect
-from data import data_bank, db, app
+#from data import data_bank, db, app
 import pandas as pd
+from flask import Flask
+import csv
+
+with open('static/data.csv', encoding='UTF-8') as f:
+    data = [{k: str(v) for k, v in row.items()}
+        for row in csv.DictReader(f, skipinitialspace=True)]
+
+app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def main():
+    '''
     dictionary = [u.__dict__ for u in db.session.query(data_bank).all()]
     for i in dictionary:
         del i['_sa_instance_state']
     df = pd.DataFrame(dictionary)
     dfJSON = df.to_json(orient='index')
-    return render_template('index.html', dfJSON=dfJSON)
+    :return:
+    '''
+
+    return render_template('main.html', dfJSON=None, data=data)
 
 
 if __name__ == "__main__":
--- converter.py
+++ converter.py
@@ -2,6 +2,6 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 
-engine = create_engine('sqlite:///static/data.db')
-data = pd.read_csv('static/data.csv')
-data.to_sql('data', engine, if_exists='replace', index=False)
+#engine = create_engine('sqlite:///static/data.db')
+#data = pd.read_csv('static/data.csv')
+#data.to_sql('data', engine, if_exists='replace', index=False)
