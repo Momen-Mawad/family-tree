d853281b2f581f48ff11e68ab7c4aeab5e795935
Momen <eng.momen3@gmail.com>
2021-06-15 16:47:40 +0200

    added new members

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from flask import render_template, url_for, redirect
-#from data import data_bank, db, app
-import pandas as pd
+from flask import render_template
 from flask import Flask
 import csv
 
@@ -8,19 +6,20 @@ with open('static/data.csv', encoding='UTF-8') as f:
     data = [{k: str(v) for k, v in row.items()}
         for row in csv.DictReader(f, skipinitialspace=True)]
 
+for i in data:
+    if i["tags"] == 'partner':
+        i["tags"] = [i["tags"]]
+    if i["tags"] == '':
+        i.pop("tags")
+
+for i in data:
+    print(i)
+
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def main():
-    '''
-    dictionary = [u.__dict__ for u in db.session.query(data_bank).all()]
-    for i in dictionary:
-        del i['_sa_instance_state']
-    df = pd.DataFrame(dictionary)
-    dfJSON = df.to_json(orient='index')
-    :return:
-    '''
 
     return render_template('main.html', dfJSON=None, data=data)
 
